bcc129afa0e066724dc34d8fd29936c8005a26fc
seungheee <lsh3431@gmail.com>
2024-03-18 23:04:42 +0900

    feat: user controller test (user save)
    - user save 테스트 코드

--- src/main/java/com/example/test/controller/UserControllerTest.java
+++ src/main/java/com/example/test/controller/UserControllerTest.java
@@ -1,18 +1,21 @@
 package com.example.test.controller;
 
+import com.example.test.controller.userDto.UserCraeteReqDTO;
+import com.example.test.controller.userDto.UserCreateResDTO;
 import com.example.test.controller.userDto.UserGetResDTO;
 import com.example.test.service.UserService;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.time.LocalDateTime;
-
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -46,8 +49,37 @@ public class UserControllerTest {
             .andDo(print());
 
     verify(userService.getUser(userId));
-
   }
 
+  @Test
+  @DisplayName("user 생성 테스트")
+  void saveUserTest() throws Exception {
+    given(userService.saveUser(new UserCraeteReqDTO("test", "name", "1234")))
+            .willReturn(UserCreateResDTO.builder()
+                    .id("test")
+                    .name("name")
+                    .build());
+
+    UserCraeteReqDTO userDto = UserCraeteReqDTO.builder()
+            .id("test")
+            .name("name")
+            .password("1234")
+            .build();
+
+    Gson gson = new Gson();
+    String content = gson.toJson(userDto);
+
+    mockMvc.perform(
+            post("/api/user")
+                    .content(content)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").exists())
+            .andExpect(jsonPath("$.name").exists())
+            .andExpect(jsonPath("$.password").exists())
+            .andDo(print());
+
+    verify(userService.saveUser(new UserCraeteReqDTO("test", "name", "1234")));
+  }
 
 }
